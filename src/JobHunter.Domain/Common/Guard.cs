10ff8ff289c2dd8774936173e7b3f56b0de0344b
Mohamed Amir DARWISH <mohamed.amir.darwish@gmail.com>
2026-09-19 00:55:32 +0200

    Add Guard class with ValidHttpUrl method for URI checks

---
+++ src/JobHunter.Domain/Common/Guard.cs
@@ -0,0 +1,22 @@
+﻿namespace JobHunter.Domain.Common
+{
+    public static class Guard
+    {
+        public static Uri ValidHttpUrl(Uri? value, string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            if (!value.IsAbsoluteUri ||
+                (value.Scheme != Uri.UriSchemeHttp &&
+                 value.Scheme != Uri.UriSchemeHttps))
+            {
+                throw new ArgumentException("URL must be a valid HTTP or HTTPS absolute URI.", parameterName);
+            }
+
+            return value;
+        }
+    }
+}
